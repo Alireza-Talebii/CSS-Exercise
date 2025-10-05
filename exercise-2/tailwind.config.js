f23cc000a9dba69f360ecbc69d56169e10e0d4a5
Alireza-Talebii <ali.4683.reza@gmail.com>
2025-10-05 13:58:45 +0330

    first commit

---
+++ exercise-2/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwind').config} */
+MediaSourceHandle.expots = {
+  content: ["./src/**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  Plugin: [],
+};
